392d0ed1fdff7735ea44362c2343be795f773e5c
Shania <shaniae.astagina@gmail.com>
2019-03-30 17:48:49 +0700

    split algorithm

--- Pacman.py
+++ Pacman.py
@@ -1,46 +1,11 @@
-from queue import PriorityQueue
-from map.map import Map
-
-def dfs(graph, start, goal):
-    visited = []
-    path = [start]
-    fringe = PriorityQueue()
-    fringe.put((0, start, path, visited))
-    while not fringe.empty():
-        depth, current_node, path, visited = fringe.get()
-        if current_node == goal:
-            return path + [current_node]
-        visited = visited + [current_node]
-        child_nodes = graph[current_node]
-        for node in child_nodes:
-            if node not in visited:
-                if node == goal:
-                    return path + [node]
-                depth_of_node = len(path)
-                fringe.put((-depth_of_node, node, path + [node], visited))
-
-    return path
-
-def bfs(graph, start, goal):
-    visited = []
-    queue = [[start]]
-
-    while queue:
-        path = queue.pop(0)
-        node = path[-1]
-        if node not in visited:
-            neighbours = graph[node]
-            for neighbour in neighbours:
-                new_path = list(path)
-                new_path.append(neighbour)
-                queue.append(new_path)
-                if neighbour == goal:
-                    return new_path
-            visited.append(node)
+from Util.Algorithms import Algorithm
+from Map.map import Map
 
 if __name__ == "__main__":
     map1 = Map.graph
     map2 = Map.graph1
+    dfs = Algorithm.dfs()
+    bfs = Algorithm.bfs()
 
     path = dfs(map1, (1, 1), (6, 6))
     path2 = dfs(map1, (1, 1), (2, 3))
---
+++ Util/Algorithms.py
@@ -0,0 +1,39 @@
+from queue import PriorityQueue
+
+class Algorithm:
+    def dfs(graph, start, goal):
+        visited = []
+        path = [start]
+        fringe = PriorityQueue()
+        fringe.put((0, start, path, visited))
+        while not fringe.empty():
+            depth, current_node, path, visited = fringe.get()
+            if current_node == goal:
+                return path + [current_node]
+            visited = visited + [current_node]
+            child_nodes = graph[current_node]
+            for node in child_nodes:
+                if node not in visited:
+                    if node == goal:
+                        return path + [node]
+                    depth_of_node = len(path)
+                    fringe.put((-depth_of_node, node, path + [node], visited))
+
+        return path
+
+    def bfs(graph, start, goal):
+        visited = []
+        queue = [[start]]
+
+        while queue:
+            path = queue.pop(0)
+            node = path[-1]
+            if node not in visited:
+                neighbours = graph[node]
+                for neighbour in neighbours:
+                    new_path = list(path)
+                    new_path.append(neighbour)
+                    queue.append(new_path)
+                    if neighbour == goal:
+                        return new_path
+                visited.append(node)
